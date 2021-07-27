ef203d92236d0d74dc9c8842fccf80792c3e6199
SOLTANI Mohamed Jihed <87778186+SOLTANIMohamedjihed@users.noreply.github.com>
2021-07-27 18:10:16 +0100

    Initial commit

---
+++ main.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(ggplot2)
+library(rjson)
+library(jsonlite)
+library(leaflet)
+library(RCurl)
+
+r_birthplace_map <- leaflet() %>%
+  addTiles() %>%  # use the default base map which is OpenStreetMap tiles
+  addMarkers(lng=10.181665, lat=36.799904,
+             popup="The birthplace of R")
+r_birthplace_map
+
+
+
+
+
+
+
+
